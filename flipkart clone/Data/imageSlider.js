c05ee42e50db0ff69739e3b1a2849155da374cd5
Abhisheklikho <111559519+Abhisheklikho@users.noreply.github.com>
2023-06-20 21:29:08 +0530

    Add files via upload

---
+++ flipkart clone/Data/imageSlider.js
@@ -0,0 +1,22 @@
+export const imageSlider = [
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/fde015eb868d156c.jpg?q=50",
+        link : "https://www.flipkart.com/infinix-note-30-5g-gh67-store?PARAM=16536253&otracker=hp_bannerads_2_2.bannerAdCard.BANNERADS_A_2R1MAW4Z9CM4"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/bada3904b2749464.jpg?q=50",
+        link : "https://www.flipkart.com/travel/flights?param=FKHPtraveldthpwflynew20offintl&otracker=hp_bannerads_1_2.bannerAdCard.BANNERADS_a_EXYOG6R04JTJ"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/a1d93b6bc446790d.jpg?q=50",
+        link : "https://www.flipkart.com/travel/flights?param=Travel_FKHP_DT-HPW_FLYFK-18off-dom-fli&otracker=hp_bannerads_3_2.bannerAdCard.BANNERADS_a_SSPU25DMHJ9K"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/d455ac035a3e41f0.jpg?q=50",
+        link : "https://www.flipkart.com/furniture/mattresses/~cs-nzao692ux1/pr?sid=wwe%2Crg9&p%5B%5D=facets.price_range.from%3D3000&p%5B%5D=facets.price_range.to%3DMax&otracker=hp_bannerads_4_2.bannerAdCard.BANNERADS_Mattres_W4Z6JG57JH76"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/bada3904b2749464.jpg?q=50",
+        link : "https://www.flipkart.com/travel/flights?param=FKHPtraveldthpwflynew20offintl&otracker=hp_bannerads_1_2.bannerAdCard.BANNERADS_a_EXYOG6R04JTJ"
+    },
+]
